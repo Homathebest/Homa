687622ee9dd8cc402dfbecea43a612ff3eccf140
Homathebest <30778519+Homathebest@users.noreply.github.com>
2021-03-04 16:56:41 +0300

    Update text.py

--- text.py
+++ text.py
@@ -1,9 +1,4 @@
 import random
 colors = random.choice(['\033[1;32m'])
-banner = colors+"""
-█▀▀ █▀▄▀█ █▀▀ █▀▀▄ █▀▀█ █░░█ █▀▀ █▀▀█ 
-▀▀█ █░▀░█ ▀▀█ █▀▀▄ █▄▄█ █▀▀█ █▀▀ █▄▄▀ 
-▀▀▀ ▀░░░▀ ▀▀▀ ▀▀▀░ ▀░░▀ ▀░░▀ ▀▀▀ ▀░▀▀
-"""
 
 cursor = colors+"sms.baher>> "
